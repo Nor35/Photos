35c01c8cefc229d31aa7616942a729638128e5b9
DESKTOP-G7UOOD1\Lepshi <podluzhnypavel@gmail.com>
2022-03-18 18:31:54 +0300

    Add Navigation to module feature_album with feature modules

---
+++ app/src/main/java/com/nor35/photos/presentation/NavHostActivity.kt
@@ -0,0 +1,21 @@
+package com.nor35.photos.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.nor35.photos.R
+
+class NavHostActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
